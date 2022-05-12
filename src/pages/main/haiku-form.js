101396504a1f86b238e9101a6317d17cc281a82c
rumyancev <jsruvi@gmail.com>
2022-05-12 10:38:27 +0300

    fix form style

--- src/pages/main/haiku-form.js
+++ src/pages/main/haiku-form.js
@@ -2,43 +2,50 @@ import React from 'react'
 import {useHaikuList} from "../../hooks";
 import {withAuthGuard} from "../../hocs";
 
+const formStyle = {
+  padding: '30px 20px'
+}
+
+const formLayoutStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 20,
+}
+
+const textareaStyle = {
+  width: '100%',
+  height: '300px',
+  resize: 'none',
+  padding: '20px'
+}
+
 export const HaikuForm = withAuthGuard(() => {
   const {haikuList, addHaiku, buyHaiku} = useHaikuList();
 
   return (
     <>
       <main>
-        <form onSubmit={async event => {
+        <form style={formStyle} onSubmit={async event => {
           event.preventDefault()
           const { text } = event.target.elements
 
           addHaiku(text.value)
         }}>
-          <fieldset id="fieldset">
-            <label
-              htmlFor="text"
-              style={{
-                display: 'block',
-                color: 'var(--gray)',
-                marginBottom: '0.5em'
-              }}
-            >
-              Change text
-            </label>
-            <div style={{ display: 'flex' }}>
-              <input
-                autoComplete="off"
-                id="text"
-                style={{ flex: 1 }}
-              />
-              <button
-                style={{ borderRadius: '0 5px 5px 0' }}
-              >
-                Save
-              </button>
-            </div>
-          </fieldset>
-          <pre>{JSON.stringify(haikuList, null, '  ')}</pre>
+          <div style={formLayoutStyle} >
+            <textarea
+              autoComplete="off"
+              placeholder='Haiku text'
+              style={textareaStyle}
+              id="text"
+            />
+            <input
+              autoComplete="off"
+              placeholder='Price'
+              id="price"
+            />
+            <button>Add haiku</button>
+          </div>
+          {/*<pre>{JSON.stringify(haikuList, null, '  ')}</pre>*/}
         </form>
         <button
           onClick={() => {
